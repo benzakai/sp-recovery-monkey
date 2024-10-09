56b62009fad569d0d5f542600f67efca6eec2f43
Ashfaque <ashfaqueamk6@gmail.com>
2024-10-09 20:01:03 +0530

    billing option

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -6,12 +6,17 @@ import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { NavMenu } from "@shopify/app-bridge-react";
 import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 
-import { authenticate } from "../shopify.server";
+import { authenticate, MONTHLY_PLAN } from "../shopify.server";
 
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+  const { billing } = await authenticate.admin(request);
+  await billing.require({
+    plans: [MONTHLY_PLAN],
+    isTest: true,
+    onFailure: async () => billing.request({ plan: MONTHLY_PLAN }),
+  });
 
   return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
 };
--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -3,10 +3,13 @@ import {
   ApiVersion,
   AppDistribution,
   shopifyApp,
+  BillingInterval,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { restResources } from "@shopify/shopify-api/rest/admin/2024-07";
 import prisma from "./db.server";
+
+export const MONTHLY_PLAN = 'Monthly subscription';
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -24,6 +27,14 @@ const shopify = shopifyApp({
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
+  billing: {
+    [MONTHLY_PLAN]: {
+      amount: 49, // Monthly charge
+      currencyCode: 'USD',
+      interval: BillingInterval.Every30Days,
+      trialDays: 7, // Set trial period to 7 days
+    },
+  },
 });
 
 export default shopify;
